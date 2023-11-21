602953525127bebeb76e684e161994f1eece5291
PatiLetsPlay <86711102+PatiLetsPlay@users.noreply.github.com>
2023-11-21 19:41:01 +0100

    Poker fertig

--- Poker/Poker.py
+++ Poker/Poker.py
@@ -1,24 +1,9 @@
 import random
 import random as rand
-
-wie_viel = 5
-anzahl_ziehungen = 1000000
-
-counter = {
-    "counter_highcard": 0,
-    "counter_paar": 0,
-    "counter2_paar": 0,
-    "counter_drilling": 0,
-    "counter_straight": 0,
-    "counter_flush": 0,
-    "counter_full_house": 0,
-    "counter_vierling": 0,
-    "counter_straight_flush": 0,
-    "counter_royale_flush": 0
-}
+import sys
 
 
-def karten_ziehen(wie_viele):
+def karten_ziehen(wie_viele, counter):
     alle_gezogenen = []
     alle = list(range(0, 52))
     gezogen = random.sample(alle, wie_viele)
@@ -33,28 +18,74 @@ def karten_ziehen(wie_viele):
 
     # print(str(alle_gezogenen) + "\n")
 
-    check_hand(alle_gezogenen)
+    check_hand(alle_gezogenen, counter)
     return
 
 
-def check_hand(alle_gezogen):
+def check_hand(alle_gezogen, counter):
     if royale_flush(alle_gezogen):
+        counter["counter_royale_flush"] = counter.get("counter_royale_flush", 0) + 1
+        counter["counter_straight_flush"] = counter.get("counter_straight_flush", 0) + 1
         pass
     elif straight_flush(alle_gezogen):
+        counter["counter_straight_flush"] += 1
         pass
-    elif fullhouse_vierling(alle_gezogen):
+    elif vierling(alle_gezogen):
+        counter["counter_vierling"] = counter.get("counter_vierling", 0) + 1
+        pass
+    elif fullhouse(alle_gezogen):
+        counter["counter_full_house"] = counter.get("counter_full_house", 0) + 1
         pass
     elif flush(alle_gezogen):
+        counter["counter_flush"] = counter.get("counter_flush", 0) + 1
         pass
     elif straight(alle_gezogen):
+        counter["counter_straight"] += 1
         pass
-    elif paar_2paar_drilling(alle_gezogen):
+    elif drilling(alle_gezogen):
+        counter["counter_drilling"] = counter.get("counter_drilling", 0) + 1
+        pass
+    elif zweipaar(alle_gezogen):
+        counter["counter2_paar"] = counter.get("counter2_paar", 0) + 1
+        pass
+    elif paar(alle_gezogen):
+        counter["counter_paar"] = counter.get("counter_paar", 0) + 1
         pass
     else:
         counter["counter_highcard"] = counter.get("counter_highcard", 0) + 1
 
 
-def paar_2paar_drilling(alle_gezogen):
+def paar(alle_gezogen):
+    wie_oft_zahl_gezogen = [0 for _ in range(0, 13)]
+
+    # alleGezogen wird durchgegangen und an der Stelle der gezogenene Zahl um 1 erhöht
+    # gezogene Zahl 3 am Index 3 um 1 erhöhen
+    for gezogen in alle_gezogen:
+        wie_oft_zahl_gezogen[gezogen[1]] += 1
+    wie_oft_zahl_gezogen.sort(reverse=True)
+
+    # Paar
+    if 2 in wie_oft_zahl_gezogen:
+        return True
+    return False
+
+
+def zweipaar(alle_gezogen):
+    wie_oft_zahl_gezogen = [0 for _ in range(0, 13)]
+
+    # alleGezogen wird durchgegangen und an der Stelle der gezogenene Zahl um 1 erhöht
+    # gezogene Zahl 3 am Index 3 um 1 erhöhen
+    for gezogen in alle_gezogen:
+        wie_oft_zahl_gezogen[gezogen[1]] += 1
+    wie_oft_zahl_gezogen.sort(reverse=True)
+
+    # Zwei Paar
+    if wie_oft_zahl_gezogen[0] == 2 and wie_oft_zahl_gezogen[1] == 2:
+        return True
+    return False
+
+
+def drilling(alle_gezogen):
     # liste die 13 Felder hat die alle mit 0 belegt werden
     wie_oft_zahl_gezogen = [0 for _ in range(0, 13)]
 
@@ -66,19 +97,7 @@ def paar_2paar_drilling(alle_gezogen):
 
     # Drilling
     if 3 in wie_oft_zahl_gezogen:
-        counter["counter_drilling"] = counter.get("counter_drilling", 0) + 1
         return True
-
-    # Zwei Paar
-    elif wie_oft_zahl_gezogen[0] == 2 and wie_oft_zahl_gezogen[1] == 2:
-        counter["counter2_paar"] = counter.get("counter2_paar", 0) + 1
-        return True
-
-    # Paar
-    elif 2 in wie_oft_zahl_gezogen:
-        counter["counter_paar"] += 1
-        return True
-
     return False
 
 
@@ -95,7 +114,6 @@ def straight(alle_gezogen):
                 and wie_oft_zahl_gezogen[2 + i] == 1 \
                 and wie_oft_zahl_gezogen[3 + i] == 1 \
                 and wie_oft_zahl_gezogen[(i + 4) % 13] == 1:
-            counter["counter_straight"] += 1
             return True
 
     return False
@@ -111,14 +129,29 @@ def flush(alle_gezogen):
 
     wie_oft_farbe_gezogen.sort(reverse=True)
     if wie_oft_farbe_gezogen[0] == 5:
-        counter["counter_flush"] = counter.get("counter_flush", 0) + 1
         return True
 
     return False
 
 
-def fullhouse_vierling(alle_gezogen):
+def fullhouse(alle_gezogen):
     # liste die 13 Felder hat die alle mit 0 belegt werden
+    wie_oft_zahl_gezogen = [0 for _ in range(0, 13)]
+
+    # alleGezogen wird durchgegangen und an der Stelle der gezogenene Zahl um 1 erhöht
+    # gezogene Zahl 3 am Index 3 um 1 erhöhen
+    for gezogen in alle_gezogen:
+        wie_oft_zahl_gezogen[gezogen[1]] += 1
+    wie_oft_zahl_gezogen.sort(reverse=True)
+
+    # Full-house
+    if wie_oft_zahl_gezogen[0] == 3 and wie_oft_zahl_gezogen[1] == 2:
+        return True
+
+    return False
+
+
+def vierling(alle_gezogen):
     wie_oft_zahl_gezogen = [0 for _ in range(0, 13)]
 
     # alleGezogen wird durchgegangen und an der Stelle der gezogenene Zahl um 1 erhöht
@@ -129,12 +162,6 @@ def fullhouse_vierling(alle_gezogen):
 
     # Vierling
     if 4 in wie_oft_zahl_gezogen:
-        counter["counter_vierling"] = counter.get("counter_vierling", 0) + 1
-        return True
-
-    # Full-house
-    elif wie_oft_zahl_gezogen[0] == 3 and wie_oft_zahl_gezogen[1] == 2:
-        counter["counter_full_house"] = counter.get("counter_full_house", 0) + 1
         return True
 
     return False
@@ -158,7 +185,6 @@ def straight_flush(alle_gezogen):
             and wie_oft_zahl_gezogen[3 + i] == 1
             and wie_oft_zahl_gezogen[(i + 4) % 13] == 1) \
                 and wie_oft_farbe_gezogen[0] == 5:
-            counter["counter_straight_flush"] += 1
             return True
     return False
 
@@ -183,16 +209,35 @@ def royale_flush(alle_gezogen):
              or wie_oft_farbe_gezogen[1] == 5
              or wie_oft_farbe_gezogen[2] == 5
              or wie_oft_farbe_gezogen[3] == 5):
-        counter["counter_royale_flush"] = counter.get("counter_royale_flush", 0) + 1
-        counter["counter_straight_flush"] = counter.get("counter_straight_flush", 0) + 1
         return True
 
     return False
 
 
-for i in range(anzahl_ziehungen):
-    karten_ziehen(wie_viel)
+def main():
+    counter = {
+        "counter_highcard": 0,
+        "counter_paar": 0,
+        "counter2_paar": 0,
+        "counter_drilling": 0,
+        "counter_straight": 0,
+        "counter_flush": 0,
+        "counter_full_house": 0,
+        "counter_vierling": 0,
+        "counter_straight_flush": 0,
+        "counter_royale_flush": 0
+    }
 
-for k, v in counter.items():
-    prozent_v = (v / anzahl_ziehungen) * 100
-    print(str(k), str(prozent_v) + "%")
+    wie_viel = 5
+    anzahl_ziehungen = int(sys.argv[1])
+
+    for i in range(anzahl_ziehungen):
+        karten_ziehen(wie_viel, counter)
+
+    for k, v in counter.items():
+        prozent_v = (v / anzahl_ziehungen) * 100
+        print(str(k), str(prozent_v) + "%")
+
+
+if __name__ == "__main__":
+    main()
