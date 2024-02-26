5005c30981b0ce68f2a76b577415cdbec8903be2
PatiLetsPlay <86711102+PatiLetsPlay@users.noreply.github.com>
2024-02-26 18:24:56 +0100

    Zeitmessung mit Decorators

--- 02_Poker/Poker_ohne_Zeitmessungsdecorator.py
+++ 02_Poker/Poker_ohne_Zeitmessungsdecorator.py
@@ -1,23 +1,6 @@
 import random
 import random as rand
 import sys
-import functools
-import time
-
-
-def timer(func):
-    """Print the runtime of the decorated function"""
-
-    @functools.wraps(func)
-    def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()  # 1
-        value = func(*args, **kwargs)
-        end_time = time.perf_counter()  # 2
-        run_time = end_time - start_time  # 3
-        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
-        return value
-
-    return wrapper_timer
 
 
 def karten_ziehen(wie_viele, counter, fakehand):
@@ -234,12 +217,6 @@ def royale_flush(alle_gezogen):
     return False
 
 
-@timer
-def kartenziehung_und_auswertung(anzahl_ziehungen, wie_viel, counter):
-    for i in range(anzahl_ziehungen):
-        karten_ziehen(wie_viel, counter, None)
-
-
 def main():
     counter = {
         "counter_highcard": 0,
@@ -256,7 +233,9 @@ def main():
 
     wie_viel = 5
     anzahl_ziehungen = int(sys.argv[1])
-    kartenziehung_und_auswertung(anzahl_ziehungen, wie_viel, counter)
+
+    for i in range(anzahl_ziehungen):
+        karten_ziehen(wie_viel, counter, [(0, 1), (1, 1), (2, 1), (3, 1), (0, 4)])
 
     for k, v in counter.items():
         prozent_v = (v / anzahl_ziehungen) * 100
